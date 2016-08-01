c436b099f7b79df5b37b0e95f31e58f5d410cfac
Ewan Sheldon <ewansheldon@gmail.com>
2016-08-01 19:00:49 +0100

    added starting-with-letter

--- directory.rb
+++ directory.rb
@@ -22,12 +22,20 @@ end
 
 def print(students)
   students.each do |student|
-    puts "#{student[:name]}, (#{student[:cohort]} cohort)"
+    puts "#{students.index(student) + 1}. #{student[:name]}, (#{student[:cohort]} cohort)"
   end
 end
 
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+def print_students_starting_with(students, letter)
+  students.each do |student|
+    if student[:name].downcase.start_with?(letter)
+    puts "#{students.index(student) + 1}. #{student[:name]}, (#{student[:cohort]} cohort)"
+    end
+  end
+end
+
+def print_footer(students)
+  puts "Overall, we have #{students.count} great students"
 end
 
 students = input_students
