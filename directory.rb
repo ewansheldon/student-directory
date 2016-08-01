9b4601e9f4d1eb2cac089386171d8bd01b4ee6c4
Ewan Sheldon <ewansheldon@gmail.com>
2016-08-01 08:16:55 +0100

    notes added

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+#print title and list of students
 puts "The students of Villains Academy"
 puts "-------------"
 puts "Dr. Hannibal Lecter"
@@ -11,6 +12,7 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
+#use print so there are no line breaks
 print "Overall, we have"
 print 11
 puts " great students"
