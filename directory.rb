5cceb91980afc88b49c5646ebc89bb2c653b2d0c
Ewan Sheldon <ewansheldon@gmail.com>
2016-08-02 08:42:13 +0100

    more info about students added

--- directory.rb
+++ directory.rb
@@ -7,7 +7,22 @@ def input_students
   name = gets.chomp
 
   while !name.empty? do
-    students << {name: name, cohort: :november}
+    puts "Please enter any hobbies of the student (separated by a comma)"
+    hobbies = gets.chomp
+    puts "Please enter the country of birth of the student"
+    country_of_origin = gets.chomp
+    puts "Please enter the height of the student in centimetres"
+    height = gets.chomp
+    if hobbies.empty?
+      hobbies = "unknown"
+    end
+    if country_of_origin.empty?
+      country_of_origin = "unknown"
+    end
+    if height.empty?
+      height = "unknown"
+    end
+    students << {name: name, cohort: :november, hobbies: hobbies, country_of_origin: country_of_origin, height: height}
     puts "Now we have #{students.count} students"
     name = gets.chomp
   end
@@ -21,8 +36,10 @@ def print_header
 end
 
 def print(students)
-  students.each do |student|
-    puts "#{students.index(student) + 1}. #{student[:name]}, (#{student[:cohort]} cohort)"
+  current_index = 0
+  while students[current_index] != nil
+    puts "#{current_index + 1}. #{students[current_index][:name]}, (#{students[current_index][:cohort]})"
+    current_index +=1
   end
 end
 
