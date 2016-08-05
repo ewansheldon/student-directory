4aab4cfe3443549f0c5a9f6da5a372b21c302c4b
Ewan Sheldon <ewansheldon@gmail.com>
2016-08-05 01:28:08 +0100

    taking filename as argument from command line

--- directory.rb
+++ directory.rb
@@ -3,7 +3,7 @@
 def interactive_menu
   loop do
   print_menu
-  process(gets.chomp)
+  process(STDIN.gets.chomp)
   end
 end
 
@@ -43,11 +43,11 @@ def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
 
-  name = gets.strip
+  name = STDIN.gets.chomp
 
   while !name.empty? do
     puts "Please enter the cohort of the student"
-    cohort = gets.strip
+    cohort = STDIN.gets.chomp
 
     if cohort.empty?
       cohort = :november
@@ -61,7 +61,7 @@ def input_students
     else
     puts "Now we have #{@students.count} students - please enter another"
     end
-    name = gets.strip
+    name = STDIN.gets.chomp
   end
 end
 
@@ -97,8 +97,8 @@ def save_students
   file.close
 end
 
-def load_students
-  file = File.open("students.csv", "r")
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
   file.readlines.each do |line|
     name, cohort = line.chomp.split(',')
     @students << {name: name, cohort: cohort.to_sym}
@@ -106,4 +106,17 @@ def load_students
   file.close
 end
 
+def try_load_students
+  filename = ARGV.first
+  return if filename.nil?
+  if File.exists?(filename)
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}"
+  else
+    puts "Sorry, #{filename} doesn't exist"
+    exit
+  end
+end
+
+try_load_students
 interactive_menu
